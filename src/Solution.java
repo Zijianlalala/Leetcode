8e7c024ed4a21563079027c665998b189abd58cd
zijianwu <zijian_wu@foxmail.com>
2020-11-19 22:53:21 +0800

    新增11月记录;新增283.移动零

--- src/Solution.java
+++ src/Solution.java
@@ -1,2 +1,17 @@
 public class Solution {
+    /**
+     * 283. 移动零
+     * @param nums
+     */
+    public void moveZeroes(int[] nums) {
+        int i = 0,j = 0;
+        for (; i < nums.length; i++) {
+            if (nums[i] != 0){
+                nums[j++] = nums[i];
+            }
+        }
+        while (j < nums.length){
+            nums[j++] = 0;
+        }
+    }
 }
