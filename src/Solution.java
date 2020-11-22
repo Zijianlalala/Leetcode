07449e617dea45bb70e091c995922d8552554a3d
zijianwu <zijian_wu@foxmail.com>
2020-11-22 12:13:18 +0800

    用数组替代Hashmap

--- src/Solution.java
+++ src/Solution.java
@@ -170,17 +170,18 @@ public class Solution {
     public boolean isAnagram(String s, String t) {
         if (s == null || t == null) return false;
         if (s.length() != t.length()) return false;
-        HashMap<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        int[] map = new int[26];
+        // 初始化
+        for (int i: map
+             ) {
+            i = 0;
+        }
+         for (int i = 0; i < s.length(); i++) {
+             map[s.charAt(i)-97]++;
         }
         for (int i = 0; i < t.length(); i++) {
-            char ch = s.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0) - 1);
-            if (map.get(ch) < 0) {
-                return false;
-            }
+             if (--map[t.charAt(i)-97] < 0)
+                 return false;
         }
         return true;
     }
